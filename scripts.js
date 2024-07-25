c10515619167cf37226ffb975742d772d5d0cfcb
SN-Marcelo <149278298+SN-Marcelo@users.noreply.github.com>
2024-07-25 12:58:34 -0500

    chore: first commit

---
+++ scripts.js
@@ -0,0 +1,49 @@
+const darkButton = document.getElementById('dark');
+const lightButton = document.getElementById('light');
+const body = document.body;
+
+function openThemeDropdown(event) {
+    event.stopPropagation();
+    const dropdown = document.getElementById('theme-dropdown-selector');
+    if (dropdown.classList.contains('show')) {
+        closeDropdown(dropdown);
+    } else {
+        openDropdown(dropdown);
+    }
+}
+
+function openDropdown(dropdown) {
+    setTimeout(() => {
+        dropdown.classList.add('show');
+    }, 0);
+    dropdown.style.display = 'inline-flex';
+    document.addEventListener('click', closeDropdownOnClickOutside);
+}
+
+function closeDropdown(dropdown) {
+    dropdown.classList.remove('show');
+    setTimeout(() => {
+        dropdown.style.display = 'none';
+    }, 500);
+    document.removeEventListener('click', closeDropdownOnClickOutside);
+}
+
+function closeDropdownOnClickOutside(event) {
+    const dropdown = document.getElementById('theme-dropdown-selector');
+    if (!dropdown.contains(event.target) && dropdown.classList.contains('show')) {
+        closeDropdown(dropdown);
+    }
+}
+
+
+document.getElementById('theme-dropdown-selector').addEventListener('click', (event) => {
+    event.stopPropagation();
+});
+
+darkButton.onclick = () => {
+    body.classList.replace('light', 'dark');
+};
+
+lightButton.onclick = () => {
+    body.classList.replace('dark', 'light');
+};
