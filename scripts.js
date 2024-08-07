9da629cddd32e9b6facb249bf7db4eaf1facab8d
SN-Marcelo <149278298+SN-Marcelo@users.noreply.github.com>
2024-08-07 17:20:06 -0500

    feat: sidebar

--- scripts.js
+++ scripts.js
@@ -55,3 +55,13 @@ lightButton.onclick = () => {
         element.style.backgroundPosition = 'left';
     });
 };
+
+function openSidebar(){
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "flex";
+}
+
+function closeSidebar(){
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "none";
+}
